cf448e3cfdfb87cc5d1e567532b6f18d3c7efcb9
Wesley-Santos-Front <wesleytksantos321@gmail.com>
2025-01-09 22:53:32 -0300

    Pronto

--- JS/main.js
+++ JS/main.js
@@ -1,28 +1,38 @@
 /* Declarando variaveis*/
 const modal1 = document.getElementById("card-modal");
-const modal3 = document.getElementById("card-modal3")
+const modal3 = document.getElementById("card-modal3");
+const modal4 = document.getElementById("card-modal4");
 const itens = document.getElementById("card-itens");
 const Total = document.getElementById("card-total");
-var fecha = document.getElementById("fech");
-const finalizar = document.getElementById("finaliz");
+const fecha = document.getElementById("fech");
+const proximo = document.getElementById("finaliz");
+const volt = document.getElementById("voltare");
+const final = document.getElementById("finaliza");
 const rua = document.getElementById("adress");
 const numero = document.getElementById("adress1");
-const bairro = document.getElementById("adress2");
+const bairro = document.getElementById("adress2"); 
 const erro = document.getElementById("erro-en");
+const erro2 = document.getElementById("erro-en2");
 const card =  document.getElementById("card-menu");
 const menu = document.getElementById("lista");
 const nomePedidor = document.getElementById("item-name");
 const quant = document.getElementById("item-quant");
 const val = document.getElementById("item-valor");
 const quantCarr = document.getElementById("quantCarrinho");
+const butModaliza = document.getElementById("modaliza");
 const titleEntr = document.getElementById("title-entrega");
 const nome = document.getElementById("nome1");
 const checkbox = document.getElementById("check");
 const teleen = document.getElementById("tele1");
+const obs = document.getElementById("observacao");
+const complement = document.getElementById("compl");
+const pag = document.getElementById("sele");
+const maionese = document.getElementById("maio");
 
 
 
 let cart = [];
+let teleE = 0;
 
 /*acionar o modal sim ou não*/
  
@@ -72,13 +82,13 @@ function addToCard(name, price){
 updateCardModal()
 
 }
-
     
-
 /*Função atualizar carrinho*/
 function updateCardModal(){
     nomePedidor.innerHTML="";
-    let total = 0;
+    var total = 0;
+    var total1 = 0;
+    
     cart.forEach(item =>{
         const createDiv = document.createElement("div");
     
@@ -101,27 +111,31 @@ function updateCardModal(){
         </div>
         `
         total += item.price * item.quantity;
+        total1 = total + teleE;
         nomePedidor.appendChild(createDiv);
        
 
     })
-    Total.textContent=total.toFixed(2);
+    quantCarr.innerHTML ="(" +  cart.length + ")";
+    Total.textContent=total1.toFixed(2);
 
 
 }
 
 
+
 /*Acionar modal pedido*/
-function modaliza(){
-updateCardModal()
-modal3.style.display="block"
+butModaliza.addEventListener("click", function(){
+    updateCardModal()
+    modal3.style.display="block"
+})
 
 fecha.addEventListener("click", function(){
          modal3.style.display="none"
 
 })
 
-}
+
 
 //função para remover o item do carrinho
 nomePedidor.addEventListener("click", function(event){
@@ -148,6 +162,8 @@ function removeItemCa(name1){
 //Função para pegar o input
 checkbox.addEventListener("click", function(){
     if (checkbox.checked){
+        teleE = 9;
+        updateCardModal();
         Toastify({
             text: "Será adicionado R$ 9,00 com a tele",
             duration: 2600,
@@ -172,45 +188,47 @@ rua.addEventListener("input", function(event){
     let inputValue = event.target.value;
 
     if(inputValue !== "") {
-    erro.style.visibility="hidden";
+    erro2.style.visibility="hidden";
 }
 });
   numero.addEventListener("input", function(event1){
     let inputValue = event1.target.value;
 
     if(inputValue !== ""){
-    erro.style.visibility="hidden";
+    erro2.style.visibility="hidden";
  }
 });
 bairro.addEventListener("input", function(event2){
     let inputValue = event2.target.value;
 
     if(inputValue !== ""){
-    erro.style.visibility="hidden";
+    erro2.style.visibility="hidden";
 }
-});} else {
+});
+complement.addEventListener("input", function(event3){
+    let inputValue = event3.target.value;
+
+    if(inputValue !== ""){
+        erro2.style.visibility="hidden";
+    }
+})} else {
+    teleE = 0;
+    obs.value = "";
     rua.value = "";
     numero.value = "";
     bairro.value = "";
+    complement.value = "";
     teleen.style.display="none";
-    updateCardModal();
     updateCardModal();
 }
 });
 
-//Finalizar pedido
-finalizar.addEventListener("click", function(){
-    if(checkbox.checked){
-    if(rua.value === "" || numero.value === "" || bairro.value === "") {
-        erro.style.visibility="visible";
-        return;
-    }
-} else {
+// Avançar pedido
+proximo.addEventListener("click", function(){
     if(nome.value === "") {
         erro.style.visibility="visible";
         return;
-}
-}
+    } 
 
     const isOpen = checkRestauranteOn();
 
@@ -225,52 +243,88 @@ finalizar.addEventListener("click", function(){
             },
           }).showToast();
         return;
-    }
-
+        
+        }    
     if(cart.length === 0) return;
+     modal3.style.display="none";
+        modal4.style.display="block"
 
 
-    //Enviar pedido para o Whatsapp
-    const cardItem = cart.map((item) => {
-        return (
-            `
-*- ${item.name}*, Quantidade: ${item.quantity};`
-        )
+// modal 2
 
-    }).join("");
+volt.addEventListener("click", function(){
+    modal3.style.display="block";
+    modal4.style.display="none";
+})
 
-    const endeR = cart.map((item) => {
-        return (
-`                                                                                  `
-        ) }).join("");
-
-    const message = encodeURIComponent(cardItem);
-    const ende = encodeURIComponent(endeR);
-    const fone = "5198968933"
-
-    if (checkbox.checked){
-        window.open(`https://wa.me/${fone}?text=Boa noite, me chamo ${nome.value}, Queria Pedir: ${message} ${ende} Endereço: ${rua.value}, Nº: ${numero.value}, Bairro: ${bairro.value}.`);
-    }else{
-        window.open(`https://wa.me/${fone}?text=Boa noite, me chamo ${nome.value}, Queria Pedir: ${message}`);
+final.addEventListener("click", function(){
+    if(checkbox.checked){
+        if(rua.value === "" || numero.value === "" || bairro.value === "" || complement.value ==="") {
+            erro2.style.visibility="visible";
+            return;
+        }
+    } else {
+    
     }
+    
+    
+    
+        //Enviar pedido para o Whatsapp
+        const cardItem = cart.map((item) => {
+            return (
+                `
+*- ${item.name}*, quantidade: ${item.quantity};`
+            )
+    
+        }).join("");
+    
+        const endeR = cart.map((item) => {
+            return (
+    `                                                                                                                                                                                                                                                            `
+            ) }).join("");
+           
+            const endeR1 = cart.map((item) => {
+                return (
+     `                                                                                                                                                                                                                                                            `
+                )
+        
+            }).join("");
+    
+        const message = encodeURIComponent(cardItem);
+        const ende = encodeURIComponent(endeR);
+        const fone = "5198968933"
+    
+        if (checkbox.checked){
+            window.open(`https://wa.me/${fone}?text=Boa noite, me chamo ${nome.value}, queria pedir: ${message} ${ende}*Endereço:* ${rua.value}, Nº: ${numero.value}, Bairro: ${bairro.value}, Complemento: ${complement.value};  ${endeR1}*Observação:* ${obs.value};${endeR1}*Maionese:* ${maionese.value}; ${endeR1}*Forma de Pagamento:* ${pag.value}; ${endeR1}*Total:* R$ ${Total.textContent}.`);
+        }else{
+            window.open(`https://wa.me/${fone}?text=Boa noite, me chamo ${nome.value}, queria pedir: ${message} ${ende} *Observação:* ${obs.value}; ${endeR1}*Maionese:* ${maionese.value}; ${endeR1}*Forma de Pagamento:* ${pag.value}; ${endeR1}*Total: R$ ${Total.textContent}.`);
+        }
+    
+       
+        cart=[];
+        checkbox.checked = false;
+        obs.value = null;
+        nome.value = null;
+        rua.value = null;
+        numero.value = null;
+        bairro.value = null;
+        complement.value = null;
+        teleen.style.display="none";
+        modal4.style.display="none";
+        updateCardModal()
+    
+    });
 
-   
-    cart=[];
-    checkbox.checked = false;
-    nome.value = null;
-    rua.value = null;
-    numero.value = null;
-    bairro.value = null;
-    teleen.style.display="none";
-    updateCardModal()
-
-});
+})
 
 //verificar hora
 function checkRestauranteOn(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 18 && hora < 23,59; //restaurante aberto
+    const dia = data.getDay();
+    console.log(data.getDay());
+    return dia != 1 && hora >= 18 && hora < 23; //restaurante aberto
+    
 }
 
 const itemSpan = document.getElementById("data-span");
