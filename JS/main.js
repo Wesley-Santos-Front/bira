9271215acf12066f143173b7bc1f355e71d6c4b4
Wesley-Santos-Front <wesleytksantos321@gmail.com>
2025-01-14 21:13:47 -0300

    atua

--- JS/main.js
+++ JS/main.js
@@ -274,7 +274,7 @@ proximo.addEventListener("click", function(){
 
     if(!isOpen){
         Toastify({
-            text: "Ops a lancheria está fechada!",
+            text: "Ops a lancheria está fechada!!!",
             duration: 2300,
             gravity: "top", // `top` or `bottom`
             position: "right", // `left`, `center` or `right`
